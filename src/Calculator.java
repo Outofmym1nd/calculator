980bef7d0ab27d53b4efb34de1f64963a8dd3b77
Outofmym1nd <spirit102@mail.ru>
2021-07-04 20:28:03 +0300

    02.07.2021

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,56 +1,79 @@
 import java.io.*;
+import java.util.HashMap;
 
 public class Calculator {
-
     public static void main(String[] args) throws IOException {
-
         BufferedReader calc = new BufferedReader(new InputStreamReader(System.in));
         String cl = calc.readLine();   //переменная, в которой хранится содержимое строки
-        //String znak = " ";
-        String plus = "+";
-        String minus = "-";
-        String umn = "*";
-        String del = "/";
+
+        HashMap<Integer, String> rome = new HashMap<>();
+        rome.put(1,"I");
+        rome.put(2,"II");
+        rome.put(3,"III");
+        rome.put(4,"IV");
+        rome.put(5,"V");
+        rome.put(6,"VI");
+        rome.put(7,"VII");
+        rome.put(8,"VIII");
+        rome.put(9,"IX");
+        rome.put(10,"X");
+
         int c = 0;
 
+        Rimsk rimsk = Rimsk.V;    //значение, которое мы получим с консоли
+
         //сложение
-        if (cl.indexOf(plus) != -1) {
-            int indexZ = cl.indexOf(plus);  //выясняем, каким по счету стоит знак
-            int indexB = indexZ + 1;  //рассчитываем индекс для считывания второго числа из строки
-            int a = Integer.parseInt(cl.substring(0, indexZ));  //присваиваем переменной значение первого числа из переведенной строки
-            int b = Integer.parseInt(cl.substring(indexB));  //присваиваем переменной значение второго числа из переведенной строки
+        if (cl.contains("+")) {
+            String[] numbers = cl.split("\\+");  //делим строку по символу +
+
+            if (rome.containsValue(numbers[0])&rome.containsValue(numbers[1])){  //если символы римские
+                System.out.println("Перевод в римские цифры");
+
+                String smb = (numbers[0]);
+                //rimsk.name();
+                System.out.println(rimsk.getArab());  //возвращаем символ
+            }
+
+            int a = Integer.parseInt(numbers[0]);
+            int b = Integer.parseInt(numbers[1]);
             c = a + b;
         } else
 
             //вычитание
-            if (cl.indexOf(minus) != -1) {
-                int indexZ = cl.indexOf(minus);  //выясняем, каким по счету стоит знак
-                int indexB = indexZ + 1;
-                int a = Integer.parseInt(cl.substring(0, indexZ));  //присваиваем переменной значение первого числа из переведенной строки
-                int b = Integer.parseInt(cl.substring(indexB));  //присваиваем переменной значение второго числа из переведенной строки
+            if (cl.contains("-")) {
+                String[] numbers = cl.split("-");
+                if (rome.containsValue(numbers[0])&rome.containsValue(numbers[1])){
+                    System.out.println("Перевод в римские цифры");
+                }
+                int a = Integer.parseInt(numbers[0]);
+                int b = Integer.parseInt(numbers[1]);
                 c = a - b;
             } else
 
                 //умножение
-                if (cl.indexOf(umn) != -1) {
-                    int indexZ = cl.indexOf(umn);  //выясняем, каким по счету стоит знак
-                    int indexB = indexZ + 1;
-                    int a = Integer.parseInt(cl.substring(0, indexZ));  //присваиваем переменной значение первого числа из переведенной строки
-                    int b = Integer.parseInt(cl.substring(indexB));  //присваиваем переменной значение второго числа из переведенной строки
+                if (cl.contains("*")) {
+                    String[] numbers = cl.split("\\*");
+                    if (rome.containsValue(numbers[0])&rome.containsValue(numbers[1])){
+                        System.out.println("Перевод в римские цифры");
+                    }
+                    int a = Integer.parseInt(numbers[0]);
+                    int b = Integer.parseInt(numbers[1]);
                     c = a * b;
                 } else
 
                     //деление
-                    if (cl.indexOf(del) != -1) {
-                        int indexZ = cl.indexOf(del);  //выясняем, каким по счету стоит знак
-                        int indexB = indexZ + 1;
-                        int a = Integer.parseInt(cl.substring(0, indexZ));  //присваиваем переменной значение первого числа из переведенной строки
-                        int b = Integer.parseInt(cl.substring(indexB));  //присваиваем переменной значение второго числа из переведенной строки
+                    if (cl.contains("/")) {
+                        String[] numbers = cl.split("/");
+                        if (rome.containsValue(numbers[0])&rome.containsValue(numbers[1])){
+                            System.out.println("Перевод в римские цифры");
+                        }
+                        int a = Integer.parseInt(numbers[0]);
+                        int b = Integer.parseInt(numbers[1]);
                         c = a / b;
                     } else
                         System.out.println("Вы ввели какую-то несуразицу");
 
         System.out.println(c);
-    }
 
+    }
 }
