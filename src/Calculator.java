0fabfb49d0f4c8f57d7f25387529429265fe9ae9
Outofmym1nd <spirit102@mail.ru>
2021-07-05 12:01:56 +0300

    02.07.2021

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,45 +6,44 @@ public class Calculator {
         BufferedReader calc = new BufferedReader(new InputStreamReader(System.in));
         String cl = calc.readLine();   //переменная, в которой хранится содержимое строки
 
-        HashMap<Integer, String> rome = new HashMap<>();
-        rome.put(1,"I");
-        rome.put(2,"II");
-        rome.put(3,"III");
-        rome.put(4,"IV");
-        rome.put(5,"V");
-        rome.put(6,"VI");
-        rome.put(7,"VII");
-        rome.put(8,"VIII");
-        rome.put(9,"IX");
-        rome.put(10,"X");
+        HashMap<String, Integer> rome = new HashMap<>();
+        rome.put("I", 1);
+        rome.put("II", 2);
+        rome.put("III", 3);
+        rome.put("IV", 4);
+        rome.put("V", 5);
+        rome.put("VI", 6);
+        rome.put("VII", 7);
+        rome.put("VIII", 8);
+        rome.put("IX", 9);
+        rome.put("X", 10);
 
         int c = 0;
 
-        Rimsk rimsk = Rimsk.V;    //значение, которое мы получим с консоли
+        //Rimsk rimsk = Rimsk.V;    //значение, которое мы получим с консоли
 
         //сложение
         if (cl.contains("+")) {
             String[] numbers = cl.split("\\+");  //делим строку по символу +
 
-            if (rome.containsValue(numbers[0])&rome.containsValue(numbers[1])){  //если символы римские
-                System.out.println("Перевод в римские цифры");
-
-                String smb = (numbers[0]);
-                //rimsk.name();
-                System.out.println(rimsk.getArab());  //возвращаем символ
+            if (rome.containsKey(numbers[0])&rome.containsKey(numbers[1])){  //если символы римские
+                c = rome.get(numbers[0])+rome.get(numbers[1]);  //вычисляем результат сложения, значения получаем по ключам
+                // c1 = IntToStr(rome.containsValue(c));//здесь будет перевод арабских цифр обратно в римские
+                System.out.println(c);  //выводим римскую
             }
 
-            int a = Integer.parseInt(numbers[0]);
-            int b = Integer.parseInt(numbers[1]);
-            c = a + b;
+            c = Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
         } else
 
             //вычитание
             if (cl.contains("-")) {
-                String[] numbers = cl.split("-");
-                if (rome.containsValue(numbers[0])&rome.containsValue(numbers[1])){
-                    System.out.println("Перевод в римские цифры");
+                String[] numbers = cl.split("-");  //делим строку по символу -
+                if (rome.containsKey(numbers[0])&rome.containsKey(numbers[1])){  //если символы римские
+                    c = rome.get(numbers[0])-rome.get(numbers[1]);  //вычисляем результат сложения, значения получаем по ключам
+                    //здесь будет перевод арабских цифр обратно в римские
+                    System.out.println(c);  //выводим римскую
                 }
+
                 int a = Integer.parseInt(numbers[0]);
                 int b = Integer.parseInt(numbers[1]);
                 c = a - b;
@@ -52,9 +51,11 @@ public class Calculator {
 
                 //умножение
                 if (cl.contains("*")) {
-                    String[] numbers = cl.split("\\*");
-                    if (rome.containsValue(numbers[0])&rome.containsValue(numbers[1])){
-                        System.out.println("Перевод в римские цифры");
+                    String[] numbers = cl.split("\\*");  //делим строку по символу *
+                    if (rome.containsKey(numbers[0])&rome.containsKey(numbers[1])){  //если символы римские
+                        c = rome.get(numbers[0])*rome.get(numbers[1]);  //вычисляем результат умножения, значения получаем по ключам
+                        //здесь будет перевод арабских цифр обратно в римские
+                        System.out.println(c);  //выводим римскую
                     }
                     int a = Integer.parseInt(numbers[0]);
                     int b = Integer.parseInt(numbers[1]);
@@ -63,10 +64,13 @@ public class Calculator {
 
                     //деление
                     if (cl.contains("/")) {
-                        String[] numbers = cl.split("/");
-                        if (rome.containsValue(numbers[0])&rome.containsValue(numbers[1])){
-                            System.out.println("Перевод в римские цифры");
+                        String[] numbers = cl.split("/");  //делим строку по символу /
+                        if (rome.containsKey(numbers[0])&rome.containsKey(numbers[1])){  //если символы римские
+                            c = rome.get(numbers[0])/rome.get(numbers[1]);  //вычисляем результат деления, значения получаем по ключам
+                            //здесь будет перевод арабских цифр обратно в римские
+                            System.out.println(c);  //выводим римскую
                         }
+
                         int a = Integer.parseInt(numbers[0]);
                         int b = Integer.parseInt(numbers[1]);
                         c = a / b;
