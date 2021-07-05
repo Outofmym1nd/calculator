d2e9312a6cf811734995804cca55dc3821fd7f92
Outofmym1nd <spirit102@mail.ru>
2021-07-05 16:53:37 +0300

    02.07.2021

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,10 +1,12 @@
 import java.io.*;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Calculator {
     public static void main(String[] args) throws IOException {
         BufferedReader calc = new BufferedReader(new InputStreamReader(System.in));
         String cl = calc.readLine();   //переменная, в которой хранится содержимое строки
+        cl = cl.replaceAll("\\s","");
 
         HashMap<String, Integer> rome = new HashMap<>();
         rome.put("I", 1);
@@ -18,6 +20,54 @@ public class Calculator {
         rome.put("IX", 9);
         rome.put("X", 10);
 
+        HashMap<Integer, String> arab = new HashMap<>();
+        arab.put(1, "I");
+        arab.put(2, "II");
+        arab.put(3, "III");
+        arab.put(4, "IV");
+        arab.put(5, "V");
+        arab.put(6, "VI");
+        arab.put(7, "VII");
+        arab.put(8, "VIII");
+        arab.put(9, "IX");
+        arab.put(10, "X");
+        arab.put(11, "XI");
+        arab.put(12, "XII");
+        arab.put(13, "XIII");
+        arab.put(14, "XIV");
+        arab.put(15, "XV");
+        arab.put(16, "XVI");
+        arab.put(17, "XVII");
+        arab.put(18, "XVIII");
+        arab.put(19, "XIX");
+        arab.put(20, "XX");
+        arab.put(21, "XXI");
+        arab.put(24, "XXIV");
+        arab.put(25, "XXV");
+        arab.put(27, "XXVII");
+        arab.put(28, "XXVIII");
+        arab.put(30, "XXX");
+        arab.put(32, "XXXII");
+        arab.put(35, "XXXV");
+        arab.put(36, "XXXVI");
+        arab.put(40, "XXXX");
+        arab.put(42, "XXXXII");
+        arab.put(45, "XXXXV");
+        arab.put(48, "XXXXVIII");
+        arab.put(49, "XXXXIX");
+        arab.put(50, "L");
+        arab.put(54, "LIV");
+        arab.put(56, "LVI");
+        arab.put(60, "LX");
+        arab.put(63, "LXIII");
+        arab.put(64, "LXIV");
+        arab.put(70, "LXX");
+        arab.put(72, "LXXII");
+        arab.put(80, "LXXX");
+        arab.put(81, "LXXXI");
+        arab.put(90, "LXXXX");
+        arab.put(100, "C");
+
         int c = 0;
 
         //Rimsk rimsk = Rimsk.V;    //значение, которое мы получим с консоли
@@ -28,8 +78,8 @@ public class Calculator {
 
             if (rome.containsKey(numbers[0])&rome.containsKey(numbers[1])){  //если символы римские
                 c = rome.get(numbers[0])+rome.get(numbers[1]);  //вычисляем результат сложения, значения получаем по ключам
-                // c1 = IntToStr(rome.containsValue(c));//здесь будет перевод арабских цифр обратно в римские
-                System.out.println(c);  //выводим римскую
+                //System.out.println(Integer.toString(c));  //переводим в строку, для сравнения с римскими числами
+                System.out.println(arab.get(c));  //выводим римскую
             }
 
             c = Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
@@ -41,12 +91,10 @@ public class Calculator {
                 if (rome.containsKey(numbers[0])&rome.containsKey(numbers[1])){  //если символы римские
                     c = rome.get(numbers[0])-rome.get(numbers[1]);  //вычисляем результат сложения, значения получаем по ключам
                     //здесь будет перевод арабских цифр обратно в римские
-                    System.out.println(c);  //выводим римскую
+                    System.out.println(arab.get(c));  //выводим римскую
                 }
 
-                int a = Integer.parseInt(numbers[0]);
-                int b = Integer.parseInt(numbers[1]);
-                c = a - b;
+                c = Integer.parseInt(numbers[0]) - Integer.parseInt(numbers[1]);
             } else
 
                 //умножение
@@ -55,11 +103,10 @@ public class Calculator {
                     if (rome.containsKey(numbers[0])&rome.containsKey(numbers[1])){  //если символы римские
                         c = rome.get(numbers[0])*rome.get(numbers[1]);  //вычисляем результат умножения, значения получаем по ключам
                         //здесь будет перевод арабских цифр обратно в римские
-                        System.out.println(c);  //выводим римскую
+                        System.out.println(arab.get(c));  //выводим римскую
                     }
-                    int a = Integer.parseInt(numbers[0]);
-                    int b = Integer.parseInt(numbers[1]);
-                    c = a * b;
+
+                    c = Integer.parseInt(numbers[0]) * Integer.parseInt(numbers[1]);
                 } else
 
                     //деление
@@ -68,12 +115,10 @@ public class Calculator {
                         if (rome.containsKey(numbers[0])&rome.containsKey(numbers[1])){  //если символы римские
                             c = rome.get(numbers[0])/rome.get(numbers[1]);  //вычисляем результат деления, значения получаем по ключам
                             //здесь будет перевод арабских цифр обратно в римские
-                            System.out.println(c);  //выводим римскую
+                            System.out.println(arab.get(c));  //выводим римскую
                         }
 
-                        int a = Integer.parseInt(numbers[0]);
-                        int b = Integer.parseInt(numbers[1]);
-                        c = a / b;
+                        c = Integer.parseInt(numbers[0]) / Integer.parseInt(numbers[1]);
                     } else
                         System.out.println("Вы ввели какую-то несуразицу");
 
