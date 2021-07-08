f4d12970f9fee32bb5ff207369e4fa8adb46d5fa
Outofmym1nd <spirit102@mail.ru>
2021-07-09 00:19:11 +0300

    02.07.2021

--- src/Calculator.java
+++ src/Calculator.java
@@ -12,69 +12,9 @@ public class Calculator {
         } else {
             cl = cl.replaceAll("\\s", "");  //удаляем пробелы, если пользователь решил вводить с ними
 
-            HashMap<String, Integer> rome = new HashMap<>();
-            rome.put("I", 1);
-            rome.put("II", 2);
-            rome.put("III", 3);
-            rome.put("IV", 4);
-            rome.put("V", 5);
-            rome.put("VI", 6);
-            rome.put("VII", 7);
-            rome.put("VIII", 8);
-            rome.put("IX", 9);
-            rome.put("X", 10);
-
-            HashMap<Integer, String> arab = new HashMap<>();
-            arab.put(1, "I");
-            arab.put(2, "II");
-            arab.put(3, "III");
-            arab.put(4, "IV");
-            arab.put(5, "V");
-            arab.put(6, "VI");
-            arab.put(7, "VII");
-            arab.put(8, "VIII");
-            arab.put(9, "IX");
-            arab.put(10, "X");
-            arab.put(11, "XI");
-            arab.put(12, "XII");
-            arab.put(13, "XIII");
-            arab.put(14, "XIV");
-            arab.put(15, "XV");
-            arab.put(16, "XVI");
-            arab.put(17, "XVII");
-            arab.put(18, "XVIII");
-            arab.put(19, "XIX");
-            arab.put(20, "XX");
-            arab.put(21, "XXI");
-            arab.put(24, "XXIV");
-            arab.put(25, "XXV");
-            arab.put(27, "XXVII");
-            arab.put(28, "XXVIII");
-            arab.put(30, "XXX");
-            arab.put(32, "XXXII");
-            arab.put(35, "XXXV");
-            arab.put(36, "XXXVI");
-            arab.put(40, "XXXX");
-            arab.put(42, "XXXXII");
-            arab.put(45, "XXXXV");
-            arab.put(48, "XXXXVIII");
-            arab.put(49, "XXXXIX");
-            arab.put(50, "L");
-            arab.put(54, "LIV");
-            arab.put(56, "LVI");
-            arab.put(60, "LX");
-            arab.put(63, "LXIII");
-            arab.put(64, "LXIV");
-            arab.put(70, "LXX");
-            arab.put(72, "LXXII");
-            arab.put(80, "LXXX");
-            arab.put(81, "LXXXI");
-            arab.put(90, "LXXXX");
-            arab.put(100, "C");
-
             Validate validate1 = new Validate();
             Validate validate2 = new Validate();
-
+            String n = "Вы ввели неподходящее число, вычисление невозможно";
             //сложение
             if (cl.contains("+")) {
                 String[] numbers = cl.split("\\+");  //делим строку по символу +
@@ -82,9 +22,10 @@ public class Calculator {
                 validate2.enter = numbers[1];
                 if (validate1.checkRomeSymbol(validate1.enter)) {  //если первая строка содержит римскую цифру
                     if (validate2.checkRomeSymbol(validate2.enter)) {  //если вторая строка содержит римскую цифру
-                        System.out.println(arab.get((rome.get(numbers[0]) + rome.get(numbers[1]))));  //складываем полученные из hashmap значения римских цифр, возвращаем по арабской цифре римскую
+                        //System.out.println(arab.get((rome.get(validate1.enter) + rome.get(validate2.enter))));  //складываем полученные из hashmap значения римских цифр, возвращаем по арабской цифре римскую
+                        System.out.println(validate1.getArabMapSymbol(validate1.getRomeMapSymbol(validate1.enter) + validate2.getRomeMapSymbol(validate2.enter)));
                     } else {
-                        System.err.println("Вы ввели неподходящее число, вычисление невозможно");
+                        System.err.println(n);
                     }
                 } else if (validate1.checkArabSymbol(validate1.enter)) {   //если первая строка содержит арабскую цифру
                     if (validate2.checkArabSymbol(validate2.enter)) {  //если вторая строка содержит арабскую цифру
@@ -92,7 +33,7 @@ public class Calculator {
                     }
                     //if (((Integer.parseInt(numbers[0]) > 0) & (Integer.parseInt(numbers[0]) <= 10)) & ((Integer.parseInt(numbers[1]) > 0) & (Integer.parseInt(numbers[1])) <= 10)) {  //проверяем по диапазону 1-10
                     else {
-                        System.err.println("Вы ввели неподходящее число, вычисление невозможно");
+                        System.err.println(n);
                     }
                 }
             }
@@ -105,20 +46,22 @@ public class Calculator {
                 int c;
                 if (validate1.checkRomeSymbol(validate1.enter)) {  //если первая строка содержит римскую цифру
                     if (validate2.checkRomeSymbol(validate2.enter)) {  //если вторая строка содержит римскую цифру
-                         c = rome.get(numbers[0]) - rome.get(numbers[1]);
+                        //c = rome.get(numbers[0]) - rome.get(numbers[1]);
+                        c = validate1.getRomeMapSymbol(validate1.enter) - validate2.getRomeMapSymbol(validate2.enter);
                         if (c == 0) {
                             System.out.println("В Римской империи так считать не умели");
                         } else
-                            System.out.println(arab.get(c));  //выводим римскую
+                            //System.out.println(arab.get(c));  //выводим римскую
+                            System.out.println(validate1.getArabMapSymbol(c));
                     } else {
-                        System.err.println("Вы ввели неподходящее число, вычисление невозможно");
+                        System.err.println(n);
                     }
                 } else if (validate1.checkArabSymbol(validate1.enter)) {   //если первая строка содержит арабскую цифру
                     if (validate2.checkArabSymbol(validate2.enter)) {  //если вторая строка содержит арабскую цифру
                         //if (((Integer.parseInt(numbers[0]) > 0) & (Integer.parseInt(numbers[0]) <= 10)) & ((Integer.parseInt(numbers[1]) > 0) & (Integer.parseInt(numbers[1])) <= 10)) {  //проверяем по диапазону 1-10
                         System.out.println(Integer.parseInt(numbers[0]) - Integer.parseInt(numbers[1]));  //переводим отрезки строки до и после знака в числа, вычисляем результат}
                     } else //{
-                        System.err.println("Вы ввели неподходящее число, вычисление невозможно");
+                        System.err.println(n);
                 } //else {
             }
 
@@ -129,9 +72,10 @@ public class Calculator {
                 validate2.enter = numbers[1];
                 if (validate1.checkRomeSymbol(validate1.enter)) {  //если первая строка содержит римскую цифру
                     if (validate2.checkRomeSymbol(validate2.enter)) {  //если вторая строка содержит римскую цифру
-                        System.out.println(arab.get((rome.get(numbers[0]) * rome.get(numbers[1]))));  //получаем из hashmap значения римских цифр, возвращаем по арабской цифре римскую
+                        //System.out.println(arab.get((rome.get(numbers[0]) * rome.get(numbers[1]))));  //получаем из hashmap значения римских цифр, возвращаем по арабской цифре римскую
+                        System.out.println(validate1.getArabMapSymbol(validate1.getRomeMapSymbol(validate1.enter) * validate2.getRomeMapSymbol(validate2.enter)));
                     } else {
-                        System.err.println("Вы ввели неподходящее число, вычисление невозможно");
+                        System.err.println(n);
                     }
                 } else if (validate1.checkArabSymbol(validate1.enter)) {   //если первая строка содержит арабскую цифру
                     if (validate2.checkArabSymbol(validate2.enter)) {  //если вторая строка содержит арабскую цифру
@@ -139,7 +83,7 @@ public class Calculator {
                     }
                     //if (((Integer.parseInt(numbers[0]) > 0) & (Integer.parseInt(numbers[0]) <= 10)) & ((Integer.parseInt(numbers[1]) > 0) & (Integer.parseInt(numbers[1])) <= 10)) {  //проверяем по диапазону 1-10
                     else {
-                        System.err.println("Вы ввели неподходящее число, вычисление невозможно");
+                        System.err.println(n);
                     }
                 }
             }
@@ -151,9 +95,10 @@ public class Calculator {
                 validate2.enter = numbers[1];
                 if (validate1.checkRomeSymbol(validate1.enter)) {  //если первая строка содержит римскую цифру
                     if (validate2.checkRomeSymbol(validate2.enter)) {  //если вторая строка содержит римскую цифру
-                        System.out.println(arab.get((rome.get(numbers[0]) / rome.get(numbers[1]))));  //получаем из hashmap значения римских цифр, возвращаем по арабской цифре римскую
+                        //System.out.println(arab.get((rome.get(numbers[0]) / rome.get(numbers[1]))));  //получаем из hashmap значения римских цифр, возвращаем по арабской цифре римскую
+                        System.out.println(validate1.getArabMapSymbol(validate1.getRomeMapSymbol(validate1.enter) / validate2.getRomeMapSymbol(validate2.enter)));
                     } else {
-                        System.err.println("Вы ввели неподходящее число, вычисление невозможно");
+                        System.err.println(n);
                     }
                 } else if (validate1.checkArabSymbol(validate1.enter)) {   //если первая строка содержит арабскую цифру
                     if (validate2.checkArabSymbol(validate2.enter)) {  //если вторая строка содержит арабскую цифру
@@ -161,7 +106,7 @@ public class Calculator {
                     }
                     //if (((Integer.parseInt(numbers[0]) > 0) & (Integer.parseInt(numbers[0]) <= 10)) & ((Integer.parseInt(numbers[1]) > 0) & (Integer.parseInt(numbers[1])) <= 10)) {  //проверяем по диапазону 1-10
                     else {
-                        System.err.println("Вы ввели неподходящее число, вычисление невозможно");
+                        System.err.println(n);
                     }
                 }
             } else {
--- src/Validate.java
+++ src/Validate.java
@@ -1,9 +1,7 @@
+import java.util.HashMap;
+
 public class Validate {
     String enter;
-
-    void showString() {
-        System.out.println("Вы ввели число: " + enter);
-    }
 
     boolean checkRomeSymbol(String enter) {
         String[] rime = new String[]{"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
@@ -38,4 +36,78 @@ public class Validate {
         }
         return false;
     }
+    public boolean checkTwoParameters(String[] elements) {
+        return elements != null && elements.length == 2;
+    }
+
+    int getRomeMapSymbol(String enter) {
+        HashMap<String, Integer> rome = new HashMap<>();
+        rome.put("I", 1);
+        rome.put("II", 2);
+        rome.put("III", 3);
+        rome.put("IV", 4);
+        rome.put("V", 5);
+        rome.put("VI", 6);
+        rome.put("VII", 7);
+        rome.put("VIII", 8);
+        rome.put("IX", 9);
+        rome.put("X", 10);
+
+        int j = rome.get(enter);
+        return j;
+    }
+
+    String getArabMapSymbol(int ent) {
+        HashMap<Integer, String> arab = new HashMap<>();
+        arab.put(1, "I");
+        arab.put(2, "II");
+        arab.put(3, "III");
+        arab.put(4, "IV");
+        arab.put(5, "V");
+        arab.put(6, "VI");
+        arab.put(7, "VII");
+        arab.put(8, "VIII");
+        arab.put(9, "IX");
+        arab.put(10, "X");
+        arab.put(11, "XI");
+        arab.put(12, "XII");
+        arab.put(13, "XIII");
+        arab.put(14, "XIV");
+        arab.put(15, "XV");
+        arab.put(16, "XVI");
+        arab.put(17, "XVII");
+        arab.put(18, "XVIII");
+        arab.put(19, "XIX");
+        arab.put(20, "XX");
+        arab.put(21, "XXI");
+        arab.put(24, "XXIV");
+        arab.put(25, "XXV");
+        arab.put(27, "XXVII");
+        arab.put(28, "XXVIII");
+        arab.put(30, "XXX");
+        arab.put(32, "XXXII");
+        arab.put(35, "XXXV");
+        arab.put(36, "XXXVI");
+        arab.put(40, "XXXX");
+        arab.put(42, "XXXXII");
+        arab.put(45, "XXXXV");
+        arab.put(48, "XXXXVIII");
+        arab.put(49, "XXXXIX");
+        arab.put(50, "L");
+        arab.put(54, "LIV");
+        arab.put(56, "LVI");
+        arab.put(60, "LX");
+        arab.put(63, "LXIII");
+        arab.put(64, "LXIV");
+        arab.put(70, "LXX");
+        arab.put(72, "LXXII");
+        arab.put(80, "LXXX");
+        arab.put(81, "LXXXI");
+        arab.put(90, "LXXXX");
+        arab.put(100, "C");
+
+        String a = arab.get(ent);
+        return a;
+    }
+
 }
